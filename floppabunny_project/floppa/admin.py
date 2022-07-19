7ea9dbadfcd5d247beab835c53e9234c1a405abe
Florence Payne <2550419p@student.gla.ac.uk>
2022-07-19 11:56:52 +0100

    Set up all necessary models and updated the Necklace model to support slugs for individual necklace pages. Still need to implement image fields but have left this for now to allow for population script to be simpler. About to run and ensure everything is working as expected.

--- floppabunny_project/floppa/admin.py
+++ floppabunny_project/floppa/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from floppa.models import Necklace
+from floppa.models import Necklace, Customer, Commission
 
 # Register your models here.
 admin.site.register(Necklace)
+admin.site.register(Customer)
+admin.site.register(Commission)
